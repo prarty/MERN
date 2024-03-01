b80344a9c975a3034b3f4fa33128df65e3839545
prarty <prarty@github.com>
2024-03-01 07:38:47 +0530

    add react-redux tool kit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import {Link, useNavigate} from 'react-router-dom'
 import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {signInStart, signInError, signInSuccess} from "../redux/user/userSlice.js";
 
 export default function SignIn() {
     const [formData, setFormData] = useState({})
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const {loading, error} = useSelector((state) => state.user)
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -15,7 +17,7 @@ export default function SignIn() {
 
     const handleSubmit = async (e) => {
         e.preventDefault() //prevent page from getting refresh
-        setLoading(true);
+        dispatch(signInStart())
         try{
             const res = await fetch("/api/auth/sign-in",
                 {
@@ -26,23 +28,20 @@ export default function SignIn() {
                     body: JSON.stringify(formData)
                 }
             );
-            const resData = await res.json();
-            if(resData.success === false) {
-                setLoading(false)
-                setError(resData.errorMessage)
+            const data = await res.json();
+            if(data.success === false) {
+                dispatch(signInError(data.errorMessage))
                 return;
             }
-            setLoading(false)
-            setError(null);
+            dispatch(signInSuccess(data))
             navigate('/');
         } catch (e) {
-            setLoading(false);
-            setError(e.message)
+            dispatch(signInError(e.errorMessage))
         }
     }
     return (
         <div className='p-3 max-w-lg mx-auto'>
-            <h1 className={"text-3xl text-center font-semibold my-7"}>Sign Up</h1>
+            <h1 className={"text-3xl text-center font-semibold my-7"}>Sign In</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
                 <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
                 <input type={"password"} placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
