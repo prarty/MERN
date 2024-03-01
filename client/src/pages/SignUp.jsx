7f5c065f4888a4798fda83e6f7dcc620b460be1e
prarty <prarty@github.com>
2024-03-01 07:07:08 +0530

    complete sign-in functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -40,14 +40,13 @@ export default function SignUp() {
             setError(e.message)
         }
     }
-        console.log(formData);
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className={"text-3xl text-center font-semibold my-7"}>Sign Up</h1>
             <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
                 <input type={"text"} placeholder={"UserName"} className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-                <input type={"password"} placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
                 <input type='email' placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+                <input type={"password"} placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
                 <button
                     disabled={loading}
                     className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
